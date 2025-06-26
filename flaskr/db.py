72e51041a78a1dd193d49aada448321f2e34cae3
Victor Rodrigues <vc.crespo17@gmail.com>
2025-06-26 11:12:32 -0300

    tirando o resultado de row

--- flaskr/db.py
+++ flaskr/db.py
@@ -8,9 +8,8 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
-            detet_types = sqlite3.PARSE_DECLTYPES
+            detect_types = sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
     return g.db
 
 def close_db(e=None):
@@ -18,3 +17,18 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo('Inicialized the database')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
