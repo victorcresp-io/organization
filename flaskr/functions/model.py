f108cef5997835b73e9052e8e085f323490dc933
Victor Rodrigues <contactvictorcrespo@gmail.com>
2025-07-17 14:15:28 -0300

    Criando a lógica de WTFORMS para a rota gastos_diaria

--- flaskr/functions/model.py
+++ flaskr/functions/model.py
@@ -6,3 +6,9 @@ class DiariaUber(Form):
     valor = DecimalField('Valor da diária', validators=[DataRequired()])
     data = DateField('Data', validators=[DataRequired()])
     tempo = TimeField('Tempo total', validators=[DataRequired()])
+ 
+class GastosUber(Form):
+    descricao_gasto = StringField('Com o que foi gasto?', validators=[DataRequired()])
+    data = DateField('Informe o dia que foi feita a diária', validators=[DataRequired()])
+    gasto = DecimalField('Qual foi o valor?', validators=[DataRequired()])
+
