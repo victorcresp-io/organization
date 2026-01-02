65d2b347a70285e0dbe5252d3d3dd337dd15e063
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-02 11:28:29 -0300

    feat: adaptação do código para capturar as despesas diárias

--- flaskr/functions/model.py
+++ flaskr/functions/model.py
@@ -8,27 +8,15 @@ class DiariaUber(Form):
     tempo = TimeField('Tempo total', validators=[DataRequired()])
  
 class GastosUber(Form):
-    opcao = SelectField(
-        'Escolha uma opção',
-        choices = [
-            ('Alimentação'),
-            ('Mercado'),
-            ('Farmácia'),
-            ('Gasolina'),
-            ('Empréstimo'),
-            ('Acessórios'),
-        ],
-        validators=[DataRequired()]
-    )
     descricao_gasto = StringField('Com o que foi gasto?', validators=[DataRequired()])
     data = DateField('Informe o dia que foi feita a diária', validators=[DataRequired()])
     gasto = DecimalField('Qual foi o valor?', validators=[DataRequired()])
 
-class UberFiltering(Form):
-    data_inicial = DateField('Data inicial', validators=[DataRequired()])
-    data_final = DateField('Data final', validators=[DataRequired()])
+class Despesas(Form):
+    data_inicial = DateField('Data', validators=[DataRequired()])
+    descricao_gasto = StringField('Explique a razão desta despesa', validators=[DataRequired()])
     opcao = SelectField(
-        'Escolha uma opção',
+        'Motivo despesa',
         choices = [
             ('Alimentação'),
             ('Mercado'),
