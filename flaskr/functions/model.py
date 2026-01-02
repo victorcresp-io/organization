ef1086ddf98f3e0d14cc73f7bd10b43c54d1fabb
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-02 09:55:15 -0300

    criação do novo campo de filtro de categoria

--- flaskr/functions/model.py
+++ flaskr/functions/model.py
@@ -1,5 +1,5 @@
 
-from wtforms import Form, StringField, DateField, DecimalField, TimeField, PasswordField, BooleanField, SubmitField
+from wtforms import Form, StringField, DateField, DecimalField, TimeField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
 class DiariaUber(Form):
@@ -8,6 +8,18 @@ class DiariaUber(Form):
     tempo = TimeField('Tempo total', validators=[DataRequired()])
  
 class GastosUber(Form):
+    opcao = SelectField(
+        'Escolha uma opção',
+        choices = [
+            ('Alimentação'),
+            ('Mercado'),
+            ('Farmácia'),
+            ('Gasolina'),
+            ('Empréstimo'),
+            ('Acessórios'),
+        ],
+        validators=[DataRequired()]
+    )
     descricao_gasto = StringField('Com o que foi gasto?', validators=[DataRequired()])
     data = DateField('Informe o dia que foi feita a diária', validators=[DataRequired()])
     gasto = DecimalField('Qual foi o valor?', validators=[DataRequired()])
@@ -15,3 +27,16 @@ class GastosUber(Form):
 class UberFiltering(Form):
     data_inicial = DateField('Data inicial', validators=[DataRequired()])
     data_final = DateField('Data final', validators=[DataRequired()])
+    opcao = SelectField(
+        'Escolha uma opção',
+        choices = [
+            ('Alimentação'),
+            ('Mercado'),
+            ('Farmácia'),
+            ('Gasolina'),
+            ('Empréstimo'),
+            ('Acessórios'),
+        ],
+        validators=[DataRequired()]
+    )
+
