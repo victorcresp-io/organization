4fbe83d307ebd99e6b0965e65f853bc631117b7c
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-02 11:54:08 -0300

    feat: criação da nova tabela 'despesas' para armazenar os dados relacionados a despesas

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,14 +2,22 @@ DROP TABLE IF EXISTS uber_daily_money;
 DROP TABLE IF EXISTS uber_daily_cost;
 
 
-CREATE TABLE uber_daily_money (
+CREATE TABLE IF NOT EXISTS uber_daily_money (
     cost FLOAT,
     date TEXT,
     spent_time TEXT
 );
 
-CREATE TABLE uber_daily_cost (
+CREATE TABLE IF NOT EXISTS uber_daily_cost (
     date TEXT,
     cost REAL,
     cost_description TEXT
-)
+);
+
+CREATE TABLE IF NOT EXISTS despesas(
+    id_despesa INTEGER PRIMARY KEY,
+    data TEXT,
+    tipo_despesa TEXT,
+    explicacao_despesa TEXT,
+    item_despesa TEXT
+);
