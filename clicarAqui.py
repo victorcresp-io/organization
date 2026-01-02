facd7742bc833e67bac179a907da9a984144eb32
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-02 14:57:47 -0300

    update

--- clicarAqui.py
+++ clicarAqui.py
@@ -6,15 +6,13 @@ app = create_app()
 with app.app_context():
     conn = get_db()
     cursor = conn.cursor()
-    res = cursor.execute("SELECT date FROM uber_daily_cost WHERE date == '20/01/2001'").fetchall()
-    exemplo_saida = res[0]
+    #res = cursor.execute("SELECT date FROM uber_daily_cost WHERE date == '20/01/2001'").fetchall()
+    #exemplo_saida = res[0]
+    res = cursor.execute("""SELECT name FROM sqlite_master WHERE type = 'table'""").fetchall() 
+    exemplo_saida = res
     print(exemplo_saida)
-    print(type(exemplo_saida))
-
-'''    cursor.execute("DELETE FROM uber_daily_money")
-    conn.commit()'''
+    res_schema = cursor.execute("SELECT * FROM despesas2;").fetchall()
+    print(res_schema)
 
 
 
-'''    res = cursor.execute("""SELECT name FROM sqlite_master WHERE type = 'table'""").fetchall()
-'''    
