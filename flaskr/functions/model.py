42ee7eec3b211595bdfa1c207ff77d713ce6ad62
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-01-08 14:37:33 -0300

    feat: criação da opcao de hortifruti

--- flaskr/functions/model.py
+++ flaskr/functions/model.py
@@ -26,6 +26,7 @@ class Despesas(Form):
             ('Gasolina'),
             ('Empréstimo'),
             ('Acessórios'),
+            ('Hortifruti')
         ],
         validators=[DataRequired()]
     )
